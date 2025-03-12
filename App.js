2979cd66b3fd172056df3feb24808bc829449ae6
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-12 16:43:51 -0600

    Managing colors globally

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
 import { useState } from 'react';
 import GameScreen from './screens/GameScreen';
+import Colors from './constants/colors';
 
 export default function App() {
 
@@ -20,7 +21,7 @@ export default function App() {
 
   return (
     <LinearGradient
-      colors={['#4e0329', '#ddb52f']}
+      colors={[Colors.primary700, Colors.accent500]}
       style={styles.rootScreen}
     >
       <ImageBackground
