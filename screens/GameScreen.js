a4273c0bfd65e9820481a52c01d04b0ae919fae0
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-12 13:55:59 -0600

    Switching screens programmatically

---
+++ screens/GameScreen.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function GameScreen(params) {
+    return <Text>Game Screen</Text>
+}
+
+export default GameScreen
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
 
     const [enteredNumber, setEnteredNumber] = useState("")
 
@@ -30,7 +30,7 @@ function StartGameScreen() {
             )
             return;
         }
-        console.log("✅ Valid number.");
+        onPickNumber(chosenNumber)
 
     }
 
